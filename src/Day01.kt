c8c51ebf45ce7d07c55ef70675f0c0ffb4f2256d
Juan.salazar <juan.salazar@geniussports.com>
2022-12-04 22:34:59 -0500

    Day one challenge completed

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,43 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+
+    val listOfTotalCalories: MutableList<Int> = arrayListOf()
+
+    var i = 0
+    var elfCalories = 0
+
+    do {
+        if (input[i] == "") {
+            listOfTotalCalories.add(elfCalories)
+            elfCalories = 0
+        } else {
+            elfCalories += input[i].toInt()
+        }
+        i++
+    } while (i < input.size)
+
+    var elfWithMoreCalories = listOfTotalCalories.indices.maxByOrNull { listOfTotalCalories[it] }
+
+    println(String.format("The elf with the most calories is the %d, with a total of %d calories", elfWithMoreCalories, listOfTotalCalories[elfWithMoreCalories!!]))
+
+    val topThreeWithMostCalories: MutableList<Int> = arrayListOf()
+
+    for (x in 0..2) {
+        topThreeWithMostCalories.add(listOfTotalCalories[elfWithMoreCalories!!])
+        listOfTotalCalories.removeAt(elfWithMoreCalories)
+        elfWithMoreCalories = listOfTotalCalories.indices.maxByOrNull { listOfTotalCalories[it] }
+        println(elfWithMoreCalories)
+    }
+
+    var totalCaloriesTop3 = 0
+
+    for (topElf in topThreeWithMostCalories){
+        totalCaloriesTop3 += topElf
+    }
+
+    println(String.format("The top three elf with the most calories has a total of %d calories", totalCaloriesTop3))
+
+
+
 }
